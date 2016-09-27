3419c45a718889038e23f57b0f695737990c6a09
Christopher McCulloh <cmcculloh@exacttarget.com>
2016-09-27 13:50:17 -0400

    () fixes eslint errors

--- server.js
+++ server.js
@@ -10,35 +10,30 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 
 // Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-  // Set permissive CORS header - this allows this server to be used only as
-  // an API server in conjunction with something like webpack-dev-server.
-  res.setHeader('Access-Control-Allow-Origin', '*');
+app.use((req, res, next) => {
+	// Set permissive CORS header - this allows this server to be used only as
+	// an API server in conjunction with something like webpack-dev-server.
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Disable caching so we'll always get the latest comments.
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
+	// Disable caching so we'll always get the latest comments.
+	res.setHeader('Cache-Control', 'no-cache');
+	next();
 });
 
-
-
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+app.listen(app.get('port'), () => {
+	console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,41 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-  entry: ['webpack/hot/dev-server', './public/js/main.jsx'],
-  output: {
-    path: __dirname,
-    filename: "public/js/bundle.js"
-  },
-  resolve: {
-    extensions: [
-      '',
-      '.js',
-      '.jsx'
-    ]
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: "style!css"
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /(node_modules)/,
-        loader: 'babel', // 'babel-loader' is also a valid name to reference
-        include: [path.join(__dirname, 'public/js')],
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      { 
-        test: /\.(png|jpg|jpeg|gif)$/,
-        loader: 'file' 
-      },
-      { 
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'
-      }
-    ]
-  }
+	entry: ['webpack/hot/dev-server', './public/js/main.jsx'],
+	output: {
+		path: __dirname,
+		filename: 'public/js/bundle.js'
+	},
+	resolve: {
+		extensions: [
+			'',
+			'.js',
+			'.jsx'
+		]
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: 'style!css'
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /(node_modules)/,
+				loader: 'babel', // 'babel-loader' is also a valid name to reference
+				include: [path.join(__dirname, 'public/js')],
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif)$/,
+				loader: 'file'
+			},
+			{
+				test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				loader: 'file'
+			}
+		]
+	}
 };
